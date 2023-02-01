56acc4ca55fb00148e776fe4f4373a48b1145902
Icey <73022629+IceBearBots@users.noreply.github.com>
2023-02-01 19:31:14 +0800

    add enter item button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 const itemInput = document.getElementById("itemInput");
+const itemSubmit = document.getElementById("itemSubmit");
 
 // https://stackoverflow.com/a/4793630
 function insertAfter(referenceNode, newNode) {
@@ -7,6 +8,7 @@ function insertAfter(referenceNode, newNode) {
 }
 
 function addItem(item, checked) {
+	item = item.trim();
 	if (item.length === 0) return;
 	const div = document.createElement("div");
 	div.classList.add("item");
@@ -30,7 +32,7 @@ function addItem(item, checked) {
 	const button = document.createElement("button");
 	button.type = "button";
 	button.classList.add("itemDelete");
-	button.innerHTML = '<svg height="24" width="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>';
+	button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>';
 	button.addEventListener("click", () => {
 		checkbox.className = input.className = "";
 		osmanthusWine();
@@ -40,7 +42,7 @@ function addItem(item, checked) {
 	div.appendChild(checkbox);
 	div.appendChild(input);
 	div.appendChild(button);
-	insertAfter(itemInput, div);
+	insertAfter(document.getElementById("itemForm"), div);
 	if (typeof checked === "undefined") osmanthusWine();
 }
 
@@ -64,7 +66,11 @@ if (localStorage.getItem("data") !== null) {
 
 itemInput.addEventListener("keypress", (e) => {
 	if (e.key === "Enter") {
-		addItem(e.target.value.trim());
+		addItem(e.target.value);
 		e.target.value = "";
 	}
 });
+
+itemSubmit.addEventListener("click", () => {
+	addItem(itemInput.value);
+})
